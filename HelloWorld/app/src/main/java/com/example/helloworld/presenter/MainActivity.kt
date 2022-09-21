6a18da1047b6d4cd33841e10e330818d317a57e9
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-09-21 14:17:01 -0300

    2022-09-20: 1º Layout Realizado

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/presenter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.helloworld.presenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.helloworld.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
