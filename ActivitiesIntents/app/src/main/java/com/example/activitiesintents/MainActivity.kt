1a8e6451aa88508117c0e4b2dc7bb0625c69488d
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-09-23 16:49:58 -0300

    2022-09-23: Acitivity e Intests

--- ActivitiesIntents/app/src/main/java/com/example/activitiesintents/MainActivity.kt
+++ ActivitiesIntents/app/src/main/java/com/example/activitiesintents/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.activitiesintents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Ciclo", "onCreate")
+
+        val buttonNext = findViewById<Button>(R.id.buttonNext)
+
+        val intentSegunda = Intent(this, SegundaActivity::class.java)
+
+        buttonNext.setOnClickListener{
+            startActivity(intentSegunda)
+        }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Ciclo", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Ciclo", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Ciclo", "onPause")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Ciclo", "onDestroy")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d("Ciclo", "onStop")
     }
 }
--- ActivitiesIntents/app/src/main/java/com/example/activitiesintents/SegundaActivity.kt
+++ ActivitiesIntents/app/src/main/java/com/example/activitiesintents/SegundaActivity.kt
@@ -2,10 +2,17 @@ package com.example.activitiesintents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SegundaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
+
+        val buttonBack = findViewById<Button>(R.id.buttonBack)
+
+        buttonBack.setOnClickListener{
+            finish()
+        }
     }
 }
